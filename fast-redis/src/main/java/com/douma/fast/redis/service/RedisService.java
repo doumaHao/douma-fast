5667b915f72e37f0968172a26bbaac350ba29402
doumaHao <809103454@qq.com>
2018-12-23 01:01:17 +0800

    工程目录调整

---
+++ fast-redis/src/main/java/com/douma/fast/redis/service/RedisService.java
@@ -0,0 +1,12 @@
+package com.douma.fast.redis.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description
+ * @Author haoxijun | doumaHao
+ * @Create 2018-12-23 0:25
+ */
+@Service
+public class RedisService {
+}
